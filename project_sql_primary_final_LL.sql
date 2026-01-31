ef5f66414d9bf7e943e1d7301393f94acab09dfa
Lucia_Luptakova <lucy.luptakova@gmail.com>
2026-01-31 19:04:02 +0100

    Refactor SQL to use CTEs for primary final table

--- project_sql_primary_final_LL.sql
+++ project_sql_primary_final_LL.sql
@@ -1,67 +1,61 @@
-create table t_primary_1 as 
-select
+CREATE TABLE t_lucia_luptakova_project_SQL_primary_final AS (
+WITH t_primary_1 as ( 
+SELECT
 	cp.industry_branch_code,
-	cpib.name as name_branch,
+	cpib.name AS name_branch,
 	cp.payroll_year,
-	round(avg(cp.value):: numeric, 2) as avg_salary
-from
+	round(avg(cp.value):: numeric, 2) AS avg_salary
+FROM
 	czechia_payroll cp
-join 
+JOIN 
 	czechia_payroll_industry_branch cpib
-on 
-	cp.industry_branch_code = cpib.code
-where 
+	ON cp.industry_branch_code = cpib.code
+WHERE 
 	value_type_code = 5958
-group by 
+GROUP BY 
 	cp.industry_branch_code,
 	cpib.name,
-	cp.payroll_year;
-
-
-create table t_primary_2 as 
-select
-	name as items,
-	date_part ('year', date_from) as a_year,
-	round (avg(value):: numeric, 2) as average_prices
-from 
+	cp.payroll_year
+),
+t_primary_2 AS (
+SELECT
+	name AS items,
+	date_part ('year', date_from) AS a_year,
+	round (avg(value):: numeric, 2) AS average_prices
+FROM 
 	czechia_price cp
-join 
+JOIN 
 	czechia_price_category cpc
-on 
-	cp.category_code = cpc.code
-and
-	region_code is null
-group by 
+	ON cp.category_code = cpc.code
+	AND
+	region_code IS NULL
+GROUP BY 
 	a_year,
 	items
-order by 
+ORDER BY 
 	items,
-	a_year;
-
-create table t_primary_3 as
-select
-	year as b_year,	/*I do not like the name of collumn as name of function. I change it in my table for further using.*/
+	a_year
+),
+t_primary_3 AS (
+SELECT
+	year AS b_year,
 	gdp
-from 
+FROM 
 	economies
-where 
+WHERE 
 	country = 'Czech Republic'
-and
-	year between 2006 and 2018;
-	
-
-create table t_lucia_luptakova_project_SQL_primary_final as
-select 
+	AND year BETWEEN 2006 AND 2018
+)
+SELECT 
 	tp.*,
 	tpp.*,
 	tp3.gdp
-from
+FROM
 	t_primary_1 tp
-join 
+LEFT JOIN 
 	t_primary_2 tpp
-on 
-	tpp.a_year = tp.payroll_year
-join 
+	ON tpp.a_year = tp.payroll_year
+LEFT JOIN 
 	t_primary_3 tp3
-on 
-	tp3.b_year = tp. payroll_year;
+	ON tp3.b_year = tp. payroll_year
+);
