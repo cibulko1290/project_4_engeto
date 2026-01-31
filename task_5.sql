7396fe783ac37966c02d08a6fdcae97edd6e54c6
Lucia_Luptakova <lucy.luptakova@gmail.com>
2026-01-31 19:11:18 +0100

    Refactor SQL query for average salary and growth

--- task_5.sql
+++ task_5.sql
@@ -1,48 +1,48 @@
 /*Task 5*/
 
-with average_salary as 
-(select	
-	avg(avg_salary) as average_salary_all,
+WITH average_salary AS 
+(SELECT
+	avg(avg_salary) AS average_salary_all,
 	payroll_year,
 	gdp
-from 
+FROM
 	t_lucia_luptakova_project_SQL_primary_final
-group by
+GROUP BY 
 	payroll_year,
 	gdp
-order by  
+ORDER BY   
 	payroll_year),	
 average_percentage_growth as
-(select
+(SELECT
 	a_year,
 	items,
 	average_prices,
-	(lead(average_prices,1) over (partition by items order by a_year ) / average_prices -1) * 100 as percentage_growth_price
-from
+	(lead(average_prices,1) OVER (partition BY items ORDER BY a_year ) / average_prices -1) * 100 AS percentage_growth_price
+FROM
 	t_lucia_luptakova_project_sql_primary_final tllpspf
-group by
+GROUP BY
 	a_year,
 	items,
 	average_prices 
-order by
+ORDER BY 
 	items,
 	a_year)
-select 
-case 
-	when a_year between 2006 and 2017 then a_year + 1
-	else Null
-end as year_of_calculation,
-	lead (gdp, 1) over (order by a_year) as GDP,
-	round (avg(percentage_growth_price) :: numeric, 2) as growth_all_categories_avg,
-	round ((lead(average_salary_all ,1) over (order by a_year) / average_salary_all   -1) * 100 :: numeric, 2) as percentage_growth_salary
-from
+SELECT
+CASE 
+	WHEN a_year BETWEEN 2006 AND 2017 THEN a_year + 1
+	ELSE NULL
+END AS year_of_calculation,
+	lead (gdp, 1) OVER (ORDER BY a_year) AS GDP,
+	round (avg(percentage_growth_price) :: numeric, 2) AS growth_all_categories_avg,
+	round ((lead(average_salary_all ,1) OVER (ORDER BY a_year) / average_salary_all   -1) * 100 :: numeric, 2) AS percentage_growth_salary
+FROM
 	average_percentage_growth apg
-join average_salary asl
-on asl.payroll_year = apg.a_year
-group by 
+JOIN average_salary asl
+	ON asl.payroll_year = apg.a_year
+GROUP BY 
 	a_year,
 	average_salary_all,
 	gdp 
-order by 
+ORDER BY  
 	a_year,
 	gdp;
