603830c374dcd94aa474213b4e2a557055b36de8
Lucia_Luptakova <lucy.luptakova@gmail.com>
2026-01-31 19:06:29 +0100

    Update SQL query to compare average salaries over years
    Refactor SQL query to include a CTE for salary comparison and filter results for years 2006 to 2018.

--- task_1.sql
+++ task_1.sql
@@ -1,15 +1,38 @@
 /*Task 1*/
 
-select
+WITH base AS (
+SELECT 
 	name_branch,	
 	avg_salary,
 	payroll_year
-from 
+FROM 
 	t_lucia_luptakova_project_SQL_primary_final
-group by
+WHERE 1 = 1
+	AND payroll_year BETWEEN 2006 AND 2018
+GROUP BY
 	payroll_year,
 	name_branch,
 	avg_salary
-order by 
-	name_branch, 
-	payroll_year;
+)
+SELECT 
+	nex.payroll_year AS selected_year,
+	nex.name_branch,
+	nex.avg_salary AS avgerage_salary,
+--	nex.avg_salary - curr.avg_salary AS diff_salary,
+	CASE
+		WHEN nex.avg_salary - curr.avg_salary > 0 THEN 'Increase'
+		WHEN nex.avg_salary - curr.avg_salary < 0 then 'Decrease'
+ 		ELSE NULL 
+ 	END AS descr
+ FROM 
+ 		base curr
+ LEFT JOIN 
+ 	base nex
+ 	ON curr.name_branch = nex.name_branch
+ 	AND curr.payroll_year + 1 = nex.payroll_year
+ GROUP by
+ 	nex.payroll_year,
+ 	nex.name_branch,
+ 	nex.avg_salary,
+ 	curr.avg_salary
+HAVING nex.avg_salary - curr.avg_salary < 0;
